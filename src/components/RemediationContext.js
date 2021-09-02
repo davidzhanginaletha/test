1ad558646777bd756537699875470e126a9b2d3b
David Zhang <david.zhang@aletha.ca>
2021-09-02 14:52:52 -0400

    a

--- src/components/RemediationContext.js
+++
@@ -1,7 +0,0 @@
-import {createContext} from 'react';
-
-export const RemediationContext = createContext({
-  remediation: null,
-  setRemediation: (value) => value,
-  setDefaultRemediation: () => null,
-})
